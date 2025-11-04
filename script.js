546d2df6788a18e6ff6be58f9559e19611f1fb9d
Jubayer-Talukder-Shuvo <jtshuvo6@gmail.com>
2025-11-04 20:04:33 +0600

    finished skills section without responsive

---
+++ script.js
@@ -0,0 +1,17 @@
+function addborderGradient(className) {
+    const elements = document.querySelectorAll(`.${className}`);
+
+    elements.forEach(el => {
+    el.addEventListener('mouseenter', () => {
+        el.classList.add('borderGradient');
+    });
+
+    el.addEventListener('mouseleave', () => {
+        el.classList.remove('borderGradient');
+    });
+    });
+}
+
+
+addborderGradient('extrainfo');
+addborderGradient('skillCard');
